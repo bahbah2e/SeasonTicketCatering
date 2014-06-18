fe9ef6fc503f59db481ac9613556d99c414d1524
Patrick Toohey <ptoohey@oswego.edu>
2014-06-18 12:11:30 -0400

    nachos modal controller

---
+++ controller/nachos.js
@@ -0,0 +1,43 @@
+﻿var ModalNachos = function ($scope, $modal, $log) {
+
+    $scope.items = ['item1', 'item2', 'item3'];
+
+    $scope.nachos = function (size) {
+
+        var modalInstance = $modal.open({
+            templateUrl: 'nachos.html',
+            controller: ModalNachosInstanceCtrl,
+            size: size,
+            resolve: {
+                items: function () {
+                    return $scope.items;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+};
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+var ModalNachosInstanceCtrl = function ($scope, $modalInstance, items) {
+
+    $scope.items = items;
+    $scope.selected = {
+        item: $scope.items[0]
+    };
+
+    $scope.ok = function () {
+        $modalInstance.close($scope.selected.item);
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+};
